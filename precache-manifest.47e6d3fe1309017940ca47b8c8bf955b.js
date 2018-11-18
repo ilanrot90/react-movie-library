04d86958d8d0f1368a3e7657df11d09f0fde0cea
ilan90 <ilanrot90@gmail.com>
2018-11-18 19:08:31 +0200

    Updates

---
+++ precache-manifest.47e6d3fe1309017940ca47b8c8bf955b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "24fdd4e9ddcddd12a8f9",
+    "url": "/react-movie-library/static/css/main.2f126268.chunk.css"
+  },
+  {
+    "revision": "24fdd4e9ddcddd12a8f9",
+    "url": "/react-movie-library/static/js/main.24fdd4e9.chunk.js"
+  },
+  {
+    "revision": "bc42d7e099450297e50b",
+    "url": "/react-movie-library/static/js/1.bc42d7e0.chunk.js"
+  },
+  {
+    "revision": "a2399a7b3efca41b2e13",
+    "url": "/react-movie-library/static/js/runtime~main.a2399a7b.js"
+  },
+  {
+    "revision": "a9f034f5bb5b65fb252c212126a36dc1",
+    "url": "/react-movie-library/index.html"
+  }
+];
